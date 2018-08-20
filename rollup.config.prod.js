828d78ad0927f4bd8ec1ee022670d5f9eed9608a
Jay Freestone <jay.freestone@browsergroup.com>
2018-08-20 17:16:46 +0100

    feat(rollup): Create separate module builds.

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,18 +1,28 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
-import browsersync from 'rollup-plugin-browsersync';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: pkg.browser,
-    name: 'Limelight',
-    format: 'umd',
-    extend: true,
-    sourcemap: true,
-  },
+  output: [
+    {
+      file: pkg.browser,
+      name: 'Limelight',
+      format: 'umd',
+      extend: true,
+    },
+    {
+      file: pkg.main,
+      format: 'cjs',
+      name: 'Limelight',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
   plugins: [
     resolve(),
     commonjs({
@@ -21,9 +31,6 @@ export default {
     typescript({
       abortOnError: false,
     }),
-    browsersync({
-      server: '.',
-      files: ['dist/**/*.*', 'src/styles/*.css', '*.html'],
-    }),
+    terser(),
   ],
 };
